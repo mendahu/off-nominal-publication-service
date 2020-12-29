d5fe2e39d0329c49e531ec29343e1ad0d9472df8
Jake Robins <mendahu@gmail.com>
2020-12-29 14:08:01 -0800

    Create prototype RSS feed watcher"

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('hello');
+const Watcher = require('feed-watcher');
+const config = require('../config/config.json');
+
+const wemartiansWatcher = new Watcher(config.wemartiansPodcast.url, 60);
+
+wemartiansWatcher.on('new entries', function (entries) {
+  entries.forEach(function (entry) {
+    console.log(entry.title);
+  });
+});
+
+wemartiansWatcher
+  .start()
+  .then(function (entries) {
+    console.log('Watcher listening');
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
